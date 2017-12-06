041d0f42db983156e006fe54481b54913003327a
Charles Damasceno <swirfneblin@gmail.com>
2017-12-06 13:46:20 -0200

    update index

--- index.js
+++ index.js
@@ -1,65 +1,109 @@
 'use strict';
-const chromeLauncher = require('chrome-launcher');
-const CDP = require('chrome-remote-interface');
-const data = {
-  port: 51000
+const puppeteer = require('puppeteer');
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var params = {
+  url_initial: 'https://dominio.com',
+  frame_login: 'https://subdominio.dominio.com',
+  login_user: "user",
+  login_pass: "*****",
+  db_user_login: "user2", 
+  db_pass_login: "*****",
+  refresh_ms: 60000
 };
 
-function processUri(url,script) {
-  return new Promise(async (fulfill, reject) => {
-    const client = await CDP({ port: data.port });
-    const {Page, DOM, Runtime} = client;
-
-    await Promise.all([ Page.enable(), Runtime.enable(), DOM.enable() ]);
-    await Page.navigate({url});
-    
-    await Page.loadEventFired(async() => {
-      await setTimeout(async () => {
-        console.log('===> Loading URL: ',url)
-        for (let cmd of script){
-          console.log('        +  Execute Script: ',cmd)
-          await Runtime.evaluate({ expression: cmd });
-        }
-        console.log('\n\n');
-      }, 3000);
-      fulfill(client);
-    });
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false, 
+    ignoreHTTPSErrors: true,
+    args: [ '--start-maximized',  '--disable-infobars' ]
   });
-}
 
-async function process(objs) {
-  try {
-    const client = await Promise.resolve(processUri(objs[0].url, objs[0].scripts));
-    await setTimeout(async() => {
-      const client = await Promise.resolve(processUri(objs[1].url, objs[1].scripts));
-    }, 5000);
-  } catch (err) {
-    console.error(err);
-  }
-}
+  var page = await browser.newPage();
+  
+  console.log(`+ Acessando URL: ${params.url_initial} ...`);
+  await page.goto(params.url_initial, { waitUntil: 'networkidle2' });
+  
+  console.log(`+ Realizando login inicial ...`);
+  await page.evaluate((user, pass) => {     
+    $("input[tb-test-id='textbox-username-input']")
+    .val(user)
+    .trigger("change", { target: { value: user }}); 
+    
+    $("input[tb-test-id='textbox-password-input']")
+    .val(pass)
+    .trigger("change", { target: { value: pass } });
+    
+    $("button[tb-test-id='button-signin']").click();
+  }, params.login_user, params.login_pass);
 
-(async() => {
-  await chromeLauncher.launch({  port: data.port, chromeFlags: ['--start-maximized'] });
-  await process([ 
-  {
-    url: "https://tableau.itau/#/signin?closePopupWhenDone=true&site=Corporativo&authSetting=DEFAULT&siteLuid=&embedded=true",
-    scripts: [
-    `document.querySelector("input[tb-test-id='textbox-username-input']").value = "*****"; `,
-    `jQuery( "input[tb-test-id='textbox-username-input']" ).trigger("change", {target: {value: "*****" }}); `,
-    `document.querySelector("input[tb-test-id='textbox-password-input']").value = "*****"; `,
-    `jQuery( "input[tb-test-id='textbox-password-input']" ).trigger("change", {target: {value: "*****" }}); `,
-    `document.querySelector("button[tb-test-id='button-signin']").click(); `
-    ]
-  }, 
-  {
-    url: "https://tableau.itau/#/site/Corporativo/views/Acompanhamentodecampoonlinev2/LogsticaOutdoor_v2?:iid=1",
-    scripts: [
-    `document.querySelector("input[tb-test-id='textbox-username-input']").value = "*****"; `,
-    `jQuery( "input[tb-test-id='textbox-username-input']" ).trigger('change', {target: {value: "*****" }}); `,
-    `document.querySelector("input[tb-test-id='textbox-password-input']").value = "*****"; `,
-    `jQuery( "input[tb-test-id='textbox-password-input']" ).trigger("change", {target: {value: "*****" }}); `,
-    `document.querySelector("button[tb-test-id='button-signin']").click(); `
-    ]
-  }
-  ]);    
+  await page.waitForSelector('div.tb-tabs-banner-subplace', { timeout: 5000 });
+  await timeout(1000);
+  await page.close();
+
+  page = await browser.newPage();
+  
+  console.log(`+ Verificando iframes após login: ${params.frame_login} ...`);
+  await page.goto(params.frame_login, { waitUntil: 'networkidle2' });
+  
+  console.log(`+ Aguardando carregamento do iframe inicial ...`);
+  await page.waitForSelector('#ng-app > div > div > div.tb-bottom.ng-scope > div > div.tb-vizviewer.tb-scroll.tb-viz-with-breadcrumb-bar.ng-scope > div > div.tb-viz.ng-isolate-scope > iframe');
+  await timeout(3000);
+  
+  console.log(`+ Inicializando iframe para login na base de dados ...`);
+  let framelogin = await page.evaluate(() => {
+    return $('iframe')[0].src;
+  })
+  await page.close();
+
+  page = await browser.newPage();
+
+  console.log(`+ Carregando página para login na base de dados ...`);
+  await page.goto(framelogin, { waitUntil: 'networkidle2' });
+  await timeout(4000);
+
+  console.log(`+ Carregando contexto do iframe ...`);
+  const frame = await page.frames();
+  await timeout(4000);
+  const execcontext = await frame[1].executionContext();
+  
+  console.log(`+ Preenchendo login Database ...`);
+  await execcontext.evaluate((user, pass) => {
+    $('input[tb-test-id="textbox-username-input"]')
+    .val(user)
+    .trigger('change');
+
+    $('input[tb-test-id="textbox-password-input"]')
+    .val(pass)
+    .trigger('change');
+
+    $('#ng-app > div > div > div > form > div:nth-child(6) > span').click();
+  }, params.db_user_login, params.db_pass_login);
+
+  console.log(`+ Carregando e maximizando dashboard ...`);
+  var sizewidth = await page.evaluate(() =>  window.screen.availWidth);
+  var sizeheight = await page.evaluate(() => window.screen.availHeight);
+
+  await page.setViewport({
+    width: sizewidth, 
+    height: sizeheight, 
+    deviceScaleFactor: 1
+  });
+
+  let selectorrefreshdiv = "div[aria-label=Atualizar]";
+  await page.waitForSelector(selectorrefreshdiv);
+
+  console.log(`+ Localizado target para atualização a cada ${params.refresh_ms} ms ...`);
+  await timeout(10000);  
+  
+  setInterval(async () => {
+    let date = new Date();
+    console.log(`+ Atualizando em: ${date} ...`);
+    const inputElement = await page.$(selectorrefreshdiv);
+    await inputElement.click();
+  }, params.refresh_ms);
+  
 })();
